96b1df7cf26617fdcea0a06a68d208767a23105e
RahulPangambam <pangambamarahul@gmail.com>
2022-04-05 22:25:02 +0530

    spring

--- SpringCorePractice/src/main/java/org/npci/springcore/SpringPractice/IOCContainer.java
+++
@@ -1,9 +0,0 @@
-package org.npci.springcore.SpringPractice;
-
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-public class IOCContainer {
-
-	BeanFactory factory = new ClassPathXmlApplicationContext("\\org\\npci\\springcore\\SpringPractice\\application-context.xml");
-}
---
+++ practice/src/main/java/org/npci/spring/dependencyInjection/Addition.java
@@ -0,0 +1,8 @@
+package org.npci.spring.dependencyInjection;
+
+public class Addition {
+
+	public int add() {
+		return 30;
+	}
+}
