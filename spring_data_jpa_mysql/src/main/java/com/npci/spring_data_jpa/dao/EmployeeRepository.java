9ba86e6dae54da6a0215233f25226b4f44983f07
Ashwin Salian <salianashwin011@gmail.com>
2022-04-21 11:04:36 +0530

    Summary : Updated the database code with Put,Patch & Loggers

--- spring_data_jpa_mysql/src/main/java/com/npci/spring_data_jpa/dao/EmployeeRepository.java
+++ spring_data_jpa_mysql/src/main/java/com/npci/spring_data_jpa/dao/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.npci.spring_data_jpa.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,11 @@ import com.npci.spring_data_jpa.entity.Employee;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+	
+	
+	public void deleteByDeptName(String deptname);
+	
+	public List<Employee> findByEmpNameAndDeptName(String e ,String d);
+	
 
 }
