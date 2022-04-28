159ced02c90340bdbba5a69f632375263832595d
Ashwin Salian <salianashwin011@gmail.com>
2022-04-28 16:58:09 +0530

    Summary:Added Swagger Layer in Application

--- library_application/src/main/java/com/npci/libraryapp/LibraryApplication.java
+++ library_application/src/main/java/com/npci/libraryapp/LibraryApplication.java
@@ -2,12 +2,37 @@ package com.npci.libraryapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@EnableWebMvc
 public class LibraryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.npci.libraryapp.controller")).build()
+				.apiInfo(new ApiInfo("Library Application", "Application to manage the data of Books", "1.0",
+						"http://localhost:8080/book", "Ashwin Salian", "© 2022", "http://localhost:8080/2022"));
+	}
+
+	@Bean
+	public InternalResourceViewResolver defaultViewResolver() {
+		return new InternalResourceViewResolver();
 	}
 
 }
