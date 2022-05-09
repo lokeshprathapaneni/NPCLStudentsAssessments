b8fdbe777bf22a6b6f9fc663ea85621cbe2f3a80
Ankur Rai <aadarsh9196@gmail.com>
2022-05-09 20:36:40 +0530

    HTML CSS and JS Prac

---
+++ HtmlCssPrac/index.js
@@ -0,0 +1,9 @@
+
+function outputData(){
+    alert(
+        "First Name: "+document.getElementById('fname').value
+        +"\r\nLast Name: "+document.getElementById('lname').value
+        +"\r\nEmail: "+document.getElementById('email').value
+        +"\r\nPhone No: "+document.getElementById('phone').value
+        );
+}
