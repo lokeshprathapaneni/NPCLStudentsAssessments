cf650684b61a2529e3a800ce94a637157a317055
unknown <shuwetharameshg@gmail.com>
2022-05-11 15:52:12 +0530

    Shuwetha's repository

---
+++ FrontEnd/index.js
@@ -0,0 +1,10 @@
+
+function outputData(){
+    alert(
+        "First Name: "+document.getElementById('fname').value
+        +"\r\nLast Name: "+document.getElementById('lname').value
+        +"\r\nPhone No: "+document.getElementById('phoneno').value
+        +"\r\nEmail : "+document.getElementById('email').value
+        +"\r\nCountry: "+document.getElementById('country').value
+        );
+}
