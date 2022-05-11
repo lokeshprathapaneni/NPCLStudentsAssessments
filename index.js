60ab958c5a3e9c1129c3f278060b3a4bc030e672
Divyanshpuri <divyanshpuri02.com>
2022-05-11 18:20:44 +0530

    DivyanshPuri's Repository

---
+++ index.js
@@ -0,0 +1,11 @@
+function output() {
+    alert(
+        "First Name: " + document.getElementById('fname').value +
+        "\r\nLast Name: " + document.getElementById('lname').value +
+        "\r\nUser Name: " + document.getElementById('uname').value +
+        "\r\nPassword: " + document.getElementById('password').value +
+        "\r\nEmail: " + document.getElementById('email').value +
+        "\r\nPhone No: " + document.getElementById('phone').value +
+        "\r\nPAN NUMBER: " + document.getElementById('pan').value
+    );
+}
