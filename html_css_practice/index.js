3a976a97d0bdc71ca65814fb700abbc519989be3
Ashwin Salian <salianashwin011@gmail.com>
2022-05-22 11:53:36 +0530

    Summary : Added html css practice Code

---
+++ html_css_practice/index.js
@@ -0,0 +1,10 @@
+function modelCheck() {
+     alert(
+        "First Name: "+document.getElementById('fname').value
+        +"\r\nLast Name: "+document.getElementById('lname').value
+        +"\r\nPhone No: "+document.getElementById('mobNo').value
+        +"\r\nEmail : "+document.getElementById('email').value
+        );
+   
+}
+
