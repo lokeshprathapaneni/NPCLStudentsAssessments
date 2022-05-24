0a8c93f4c1855eb2c94da7885b28ff1ccbc7094f
Mohd saif <52712476+05saif06@users.noreply.github.com>
2022-05-24 23:37:07 +0530

    Delete EkartApp directory

--- EkartApp/src/main/java/org/ekartapp/springcore/prac/dao/EkartDao.java
+++
@@ -1,8 +0,0 @@
-package org.ekartapp.springcore.prac.dao;
-
-import org.ekartapp.springcore.prac.model.OrderTable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EkartDao extends JpaRepository<OrderTable,Integer> {
-
-}
